4190e6adc7b2160a6ecc3e2335a553eeffbaf58b
rlawlsgud980 <rlawlsgud980@naver.com>
2024-11-20 14:01:04 +0900

    first commit

---
+++ src/main/java/com/mysite/sbb/book_review/Book.java
@@ -0,0 +1,20 @@
+// package com.mysite.sbb.book_review;
+
+// import lombok.Getter;
+// import lombok.Setter;
+
+// @Getter
+// @Setter
+// public class Book {
+//     private String title;
+//     private String author;
+
+//     public static void main(String[] args){
+//         Book book = new Book();
+//         book.setTitle("title");
+//         book.setAuthor("author");
+
+//         System.out.println(book.getTitle());
+//         System.out.println(book.getAuthor());
+//     }
+// }
